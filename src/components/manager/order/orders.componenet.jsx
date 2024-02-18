e9ad9d2a0268d933132b6cc860d3b60073e7127f
shifra-fraiman <s0548549680@gmail.com>
2024-02-18 23:59:22 +0200

    changes...

--- src/components/manager/order/orders.componenet.jsx
+++ src/components/manager/order/orders.componenet.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { getMeeting, deleteMeetingById, getMeetingById, createMeeting } from "../../../service/meeting.api";
+import { getMeeting, deleteMeetingById, getMeetingById, createMeeting, } from "../../../service/meeting.api";
 import { useNavigate } from 'react-router-dom';
 import { useOrders } from "./order.context";
+import { OrderForm  } from './orderForm.component';
+ 
 
 
 export const Orders = () => {
@@ -35,13 +37,8 @@ export const Orders = () => {
         //deleteMeetingById(id);
         alert("הפגישה נמחקה בהצלחה!");
     }
-
-    const addMeeting = () => {
-        // navigate('/meeting');
-
-    }
-
-    const updateMeeting = async (e) => {
+    const addMeeting = async (e) => {
+        console.log("i in add meeting");
         e.preventDefault();
         const form = e.target;
         const data = Object.fromEntries([...(new FormData(form)).entries()]);
@@ -50,7 +47,7 @@ export const Orders = () => {
             start_time: data.time,
             duration: data.duration,
             meeting: {
-                type: service.name,
+                type: data.type,
                 date: data.date,
                 place: data.place,
                 customerDetails: {
@@ -63,75 +60,46 @@ export const Orders = () => {
         await createMeeting(meeting);
         await loadOrders();
         form.reset();
+        setFormUpdateMeetring(false);
+    }
+
+    const updateMeeting = async (e, id) => {
+        e.preventDefault();
+        const form = e.target;
+        const data = Object.fromEntries([...(new FormData(form)).entries()]);
+        const meeting = {
+            business_id: "8f571327-fd44-4f0f-b0f9-950082e0ced3",
+            start_time: data.time,
+            duration: data.duration,
+            meeting: {
+                type: data.type,
+                date: data.date,
+                place: data.place,
+                customerDetails: {
+                    firstName: data.firstName,
+                    lastName: data.lastName,
+                    phone: data.phone
+                }
+            }
+        }
+        await updateMeeting(id, meeting);
+        await loadOrders();
+        form.reset();
     }
 
 
-    const updateMeetingBtn = (meeting) => {
-        // e.preventDefault();
-        // id = e.target.id;
-        // let { data } = await getMeetingById();
+    const updateOrAddMeetingBtn = (meeting) => {
         setMeetingId(meeting);
         setFormUpdateMeetring(!formUpdateMeeting);
-
     }
 
-    return (<div width={'100%'}>
-        {orders ? <div>{orders.map(m => <li key={m.id} id={m.id}>{m.type},{m.startTime},  {m.customerDetails.firstName} {m.customerDetails.lastName}
-            <button key={m.id} id={m.id} onClick={(e) => deleteOrder(e)}>❌</button>
-            <button id={m.id} onClick={() => updateMeetingBtn(m)}>עדכון</button>
-        </li>)}</div> : <h1 color="black" >hello world </h1>}
-        <button onClick={e => addMeeting()}>להוספת פגישה</button>
-        {/* {formUpdateMeeting ? <form name="orderMeeting" onSubmit={e => updateMeeting(e)}>
-            <div>
-                <img src="src/assets/images/newBorn.JPG" /*src="{service? service.img :''}></img>
-            </div>
-            <div>
-                <label >שם פרטי
-                    <input type="text" name="firstName"></input></label>
-
-            </div>
-            <div>
-                <label >שם משפחה
-                    <input type="text" name="lastName"></input></label>
-
-            </div>
-            <div>
-                <label >טלפון
-                    <input type="tel" name="phone"></input></label>
-
-            </div>
-            <div>
-                <label >מיקום
-                    <input type="text" name="place"></input>
-                </label>
-
-            </div>
-            <div>
-                <label>בחר תאריך
-                    <input type="date" name="date"></input>
-                </label>
-
-            </div>
-            <div>
-                <label >בחר שעת התחלה
-                    <input type="time" name="time"></input>
-                </label>
-
-            </div>
-            <div>
-                <label >משך זמן - בשעות
-                    <input type="number" name="duration"></input>
-                </label>
-
-            </div>
-            <div>
-                <label >האם אתה רוצה להיכנס לרשימת הלקוחות?
-                    <input type="checkbox" name="customer"></input>
-                </label>
-            </div>
-            <button type="submit" >אישור ושליחה</button>
-        </form> : ''} */}
-
-
-    </div>)
+    return (
+        <div width={'100%'}>
+            {orders ? <div>{orders.map(m => <li key={m.id} id={m.id}>{m.type},{m.startTime},  {m.customerDetails.firstName} {m.customerDetails.lastName}
+                <button key={m.id} id={m.id} onClick={(e) => deleteOrder(e)}>❌</button>
+                <button id={m.id} onClick={() => updateOrAddMeetingBtn(m)}>עדכון</button>
+            </li>)}</div> : <h1 color="black" >hello world </h1>}
+            <button onClick={e => updateOrAddMeetingBtn(null)}>להוספת פגישה</button>
+            {formUpdateMeeting ? <OrderForm objUpdate={meetingId} updateMeeting={updateMeeting} addMeeting={addMeeting} /> : ''}
+        </div>)
 }
