256529f0129204d6786496e5af21c38a5a4f1e47
shifra-fraiman <s0548549680@gmail.com>
2024-02-19 21:41:06 +0200

    formService- changes

---
+++ src/components/manager/service/serviceForm.component.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+
+export const ServiceForm=(props)=>{
+    // let [add, setAdd]= useState(false);
+    // let [update, setUpdate]= useState(false);
+    let [service, setService]= useState('');
+    const [file, setFile] = useState();
+
+    useEffect(()=>{
+        if(props?.serviseObj)
+            setService(props.serviseObj);
+    },[])
+    
+
+    return <>{
+         <form name="orderMeeting" onSubmit={(e) =>  props.onsubmit(e, service)}>
+            <div>
+                <label >שם השירות:
+                    <input type="text" name="name" defaultValue={(service!=null)?service.name :'' }></input></label>
+
+            </div>
+            <div>
+                <label >משך זמן:
+                    <input type="text" name="duration" defaultValue={ (service!=null)?service.duration :'' }></input></label>
+
+            </div>
+            <div>
+                <label >מחיר:
+                    <input type="text" name="cost" defaultValue={(service!=null)?service.cost :'' }></input></label>
+
+            </div>
+            <div>
+                <label >קישור לתמונה:
+                    <input type="file" accept="image/*" onChange={props.handleChange} name="img"></input></label>
+                <img src={file} width={'40%'} />
+
+            </div>
+            <button type="submit" >אישור ושליחה</button>
+        </form>
+      
+    }
+    </>
+}
